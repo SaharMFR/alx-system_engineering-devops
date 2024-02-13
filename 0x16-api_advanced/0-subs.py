f7d2d2fe74bee074548bc89aea20980bcac0fab1
Sahar Mohamed <100488384+SaharMFR@users.noreply.github.com>
2024-02-13 20:01:05 +0200

    Task-0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Finds the number of subscribers for a given subreddit """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the `Reddit API` and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0.
+    """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    h = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=h, allow_redirects=False)
+    if response.status_code >= 300:
+        return 0
+    return response.json().get("data").get("subscribers")
