60fae5a58d2998d18a77fa3ba372caa77a8429fe
Sahar Mohamed <100488384+SaharMFR@users.noreply.github.com>
2024-02-13 22:59:19 +0200

    Update in task-1 0x16

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,10 +5,22 @@ import requests
 
 
 def top_ten(subreddit):
+    """
+    Queries the `Reddit API` and prints the titles of the first 10
+    hot posts listed for a given subreddit
+    """
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
 
-    h = {"User-agent": "My-User-Agent"}
+    h = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/121.0.0.0',
+            'Safari/537.36'
+        ])
+    }
     p = {"limit": 10}
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
