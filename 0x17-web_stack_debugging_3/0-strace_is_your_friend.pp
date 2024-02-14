3f5d220c43ec54d109505152983d2fa4888a972f
SaharMFR <sweetysahora@gmail.com>
2024-02-14 18:30:42 +0000

    Update in task-0 0x17

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixing Apache is returning a 500 error using `strace`.
 
 exec { 'fix_wordpress':
-  command => 'sudo sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
