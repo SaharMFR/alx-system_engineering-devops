499f54e919616819fb9221b4e6c05eeec0308a03
SaharMFR <sweetysahora@gmail.com>
2024-02-22 21:51:19 +0000

    Task-0 0x1B

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Fixing stack to not fail any requests even if they are big amount.
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
